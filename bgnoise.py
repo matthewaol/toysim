978352368190c8db1e4dc0e1c78ec9a2fcb5fd9f
matthewaol <matthewdacanay@aol.com>
2023-07-27 17:41:05 -0700

    moved bg and noise functions into bgnoise.py

---
+++ bgnoise.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+def add_background_exp(I_list, Qs, a): #add background based on exponential decay
+    background_list = []
+    
+    for q in Qs: 
+        x_comp, y_comp = q[0], q[1]
+        B = np.exp(-(x_comp**2 + y_comp**2)* a)
+        background_list.append(B)
+        
+    return np.array(background_list) * I_list
+
+def add_background_exp_no_loop(I_list, Qs, a):
+    B_list = np.exp(-(Qs[:,0]**2 + Qs[:,1]**2 )* a) * I_list
+    return B_list 
+
+def add_background_offset(I_list,a): # adds constant offset to I_list
+    return I_list + a
+
+def add_background_gaussian(I_list, mu, sigma): # adds gaussian background to I_list / increase sigma for more background
+    gaussian_background_list = np.random.default_rng().normal(mu,sigma,len(I_list))
+    return I_list + gaussian_background_list 
+
+def add_background_cauchy(I_list): #doesnt work yet
+    cauchy_list = np.random.default_rng().standard_cauchy(len(I_list))
+    return cauchy_list + I_list
+
+# Noise functions
+def add_gaussian_noise(I_list, mu, sigma): # returns the list of I's with gaussian noise multiplied into it 
+    gaussian_array = np.random.default_rng().normal(mu, sigma, len(I_list)) 
+    noisy_I_list = I_list * gaussian_array
+    return noisy_I_list 
+
+def add_poisson_noise(I_list,lam): # returns the list of I's with poisson noise multiplied into it 
+    poisson_array = np.random.default_rng().poisson(lam, len(I_list))
+    noisy_I_list = I_list * poisson_array
+    return noisy_I_list
+
+def add_saltpepper_noise(I_list,noise_level): #returns the list of I's with salt+pepper scattered in it randomly
+    
+    rng = np.random.default_rng()
+    black_or_white = [0,255] # list containing color value for black or white 
+    pixel_values = np.array(range((len(I_list)))) # list containing indices of I_list
+    
+    for i in range(noise_level): 
+        rand_index = rng.choice(pixel_values,replace=False) #random generated number with the range of the I_list size 
+        I_list[rand_index] = rng.choice(black_or_white)
+    return I_list
--- simulation.py
+++ simulation.py
@@ -8,6 +8,7 @@ import numexpr as ne
 
 from Bio.PDB.PDBParser import PDBParser
 
+import bgnoise
 import models
 import detectors
 from dxtbx.model.beam import BeamFactory
@@ -289,61 +290,6 @@ def get_I_values_no_loop_3d(Qs, Atoms, Tu, rotation_m):
     print("Finished intensities")
     return a_total.real**2 + a_total.imag**2
 
-# Background functions
-def add_background_exp(I_list, Qs, a): #add background based on exponential decay
-    background_list = []
-    
-    for q in Qs: 
-        x_comp, y_comp = q[0], q[1]
-        B = np.exp(-(x_comp**2 + y_comp**2)* a)
-        background_list.append(B)
-        
-    return np.array(background_list) * I_list
-
-def add_background_exp_no_loop(I_list, Qs, a):
-    B_list = np.exp(-(Qs[:,0]**2 + Qs[:,1]**2 )* a) * I_list
-    return B_list 
-
-def add_background_offset(I_list,a): # adds constant offset to I_list
-    return I_list + a
-
-def add_background_gaussian(I_list, mu, sigma): # adds gaussian background to I_list / increase sigma for more background
-    gaussian_background_list = np.random.default_rng().normal(mu,sigma,len(I_list))
-    return I_list + gaussian_background_list 
-
-def add_background_cauchy(I_list): #doesnt work yet
-    cauchy_list = np.random.default_rng().standard_cauchy(len(I_list))
-    return cauchy_list + I_list
-
-# Noise functions
-def add_gaussian_noise(I_list, mu, sigma): # returns the list of I's with gaussian noise multiplied into it 
-    gaussian_array = np.random.default_rng().normal(mu, sigma, len(I_list)) 
-    noisy_I_list = I_list * gaussian_array
-    return noisy_I_list 
-
-def add_poisson_noise(I_list,lam): # returns the list of I's with poisson noise multiplied into it 
-    poisson_array = np.random.default_rng().poisson(lam, len(I_list))
-    noisy_I_list = I_list * poisson_array
-    return noisy_I_list
-
-def add_saltpepper_noise(I_list,noise_level): #returns the list of I's with salt+pepper scattered in it randomly
-    
-    rng = np.random.default_rng()
-    black_or_white = [0,255] # list containing color value for black or white 
-    pixel_values = np.array(range((len(I_list)))) # list containing indices of I_list
-    
-    for i in range(noise_level): 
-        rand_index = rng.choice(pixel_values,replace=False) #random generated number with the range of the I_list size 
-        I_list[rand_index] = rng.choice(black_or_white)
-    return I_list
-
-def count_spots(I_list,square_I_list): # returns num of peaks and slices indicating the location of peaks
-    threshold = square_I_list > I_list.max() * .05 # Threshold is the upper 95% of data
-    labels, num_of_labels = sp.ndimage.label(threshold) # Label each index of intensity array based on our defined threshold
-    peaks = sp.ndimage.find_objects(labels) # gets tuples corresponding to the location of each peak
-    print("Number of spots: " + str(num_of_labels)) 
-    return num_of_labels, peaks
-
 def produce_image(pdb_file_name, Qs, num_cells,cell_size, a, degrees): 
     '''
     Produces a simulated diffraction image from 2D Q-vectors and a pdb file
@@ -364,7 +310,7 @@ def produce_image(pdb_file_name, Qs, num_cells,cell_size, a, degrees):
     molecule = get_coords_pdb(pdb_file_name, "temporary_id")
     
     I_list = get_I_values_no_loop(Qs, molecule, Tu, f_j,theta)
-    I_list_background = add_background_exp_no_loop(I_list,Qs,a)
+    I_list_background = bgnoise.add_background_exp_no_loop(I_list,Qs,a)
     
     I_list_size = int(np.sqrt(len(I_list)))
     
